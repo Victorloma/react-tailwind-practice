61b08ee9fcd787e6a1789eeac8453df3458ba94e
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-11-03 17:40:28 +0100

    added basic styling and api interaction

---
+++ src/components/ImageSearch.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ImageSearch = () => {
+  return (
+    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+      <form action="" className="w-full max-w-sm">
+        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+            
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ImageSearch;
