1912398184ccae15c3bbeb260d681a4e68c42c38
Victor Martinez <victorlorenzomartinez@gmail.com>
2022-11-04 13:53:43 +0100

    configure app for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -15,10 +16,16 @@ function App() {
         setIsLoading(false)
       })
       .catch(err => console.log(err));
-  }, [])
+  }, [term])
 
   return (
     <div className="container mx-auto">
+      <ImageSearch
+        searchText={(text) => setTerm(text)}
+      />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>}
+
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map((image) => (
           <ImageCard
